aaa431cde72b94dafbd1df06b9d02b79e38ee922
Wang Jiarui <jiarui.23@intl.zju.edu.cn>
2024-04-17 14:09:18 +0800

    Fix some problems on printing a pie chart for my dictionary

--- Practical6/Average day.py
+++ Practical6/Average day.py
@@ -1,5 +1,5 @@
 #Creating a dictionary
-my_dict = {'Sleeping':'8', 'Classes':'6', 'Studying':'8', 'TV':'2', 'Music':'1'}
+my_dict = {'Sleeping':'8', 'Classes':'6', 'Studying':'3.5', 'TV':'2', 'Music':'1'}
 #Accessing dictionary values
 print(my_dict['Sleeping']) #output: value1
 print(my_dict['Classes']) #output: value2
@@ -11,12 +11,10 @@ my_dict['Other']='3.5'
 print(my_dict['Other']) #output: value6
 print(my_dict) #output the dictionary
 import matplotlib.pyplot as plt
-#Labels and values
-activity_labels=["Sleeping","Classes","Studying","TV","Music","Other"]
-time_hours=[8,6,3.5,2,1,3.5]
-Sleeping_and_else=[0,0,0,0,0,0]
+values = list(my_dict.values())  
+labels = list(my_dict.keys()) 
 plt.figure()
-plt.pie(time_hours, labels=activity_labels, startangle=90, explode= Sleeping_and_else, autopct='%.1f%%')
+plt.pie(values, labels=labels, startangle=90, autopct='%.1f%%')
 plt.title('The average day of a university student') #Show the title
 plt.show() #show the graph
 plt.clf()
